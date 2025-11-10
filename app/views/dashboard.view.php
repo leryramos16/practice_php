7d47d30a94502a4f676679b03d8821dc1c5865ef
lery <leryramos16@gmail.com>
2025-11-10 13:08:35 +0800

    add task in notification

--- app/views/dashboard.view.php
+++ app/views/dashboard.view.php
@@ -27,6 +27,7 @@ include 'inc/header.php';
         <?php endif; ?>
       </div>
       <div class="modal-footer">
+        <a class="btn btn-primary" href="<?= ROOT?>/planner">Add task</a>
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
